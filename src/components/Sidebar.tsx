acfa6a8469be65df2ee85794bb8189fda289ffc0
fyldrmm <fyildirim@na.edu>
2026-07-23 02:26:49 +0300

    Rename sidebar's board nav item from "Job Tracker" to "Board"
    It duplicated the logo's "JobTracker" label right above it and made
    "Table" look unrelated, when it's really just another view of the same
    kanban data.

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -105,7 +105,7 @@ export function Sidebar({
       </button>
       <NavItem
         icon={<BoardIcon />}
-        label="Job Tracker"
+        label="Board"
         active={view === 'board'}
         onClick={() => onNavigate('board')}
         expanded={expanded}
